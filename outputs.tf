ba42c15330ce326e5bd68e5e6f74bfce1ee5a7f9
jlam <jlam@mayoral.es>
2021-12-15 21:42:58 +0100

    Comments added

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,8 @@
 # Write your Terraform module outputs here
 # ----------------------------------------
 
-#Terraform output values allow you to export structured data about your resources.
-#You can use this data to configure other parts of your infrastructure with automation tools, or as a data source for another Terraform workspace
+# MY COMMENTS:Terraform output values allow you to export structured data about your resources.
+# You can use this data to configure other parts of your infrastructure with automation tools, or as a data source for another Terraform workspace
 
 output "message" {
   description = "Your description about the value exported"
--- variables.tf
+++ variables.tf
@@ -2,6 +2,8 @@
 # Write your Terraform variable inputs here
 # ------------------------------------------
 
+# MY COMMENTS: variables let you customize aspects of Terraform modules without altering the module's own source code
+
 variable "zone" {
   description = "DNS zone configuration"
   type        = string

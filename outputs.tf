88f6393507d063008e46a4511454a468e9fb4f1f
jlam <jlam@mayoral.es>
2021-12-15 17:11:01 +0100

    Populating variables.tf and outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 # ----------------------------------------
 # Write your Terraform module outputs here
 # ----------------------------------------
+
+#Terraform output values allow you to export structured data about your resources.
+#You can use this data to configure other parts of your infrastructure with automation tools, or as a data source for another Terraform workspace
+
+output "message" {
+  description = "Your description about the value exported"
+  value       = "Output value to use"
+}

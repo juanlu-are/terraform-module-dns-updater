07dc846815f3d3a92648dd9ad715eace9c5d34cd
jlam <jlarellanom@outlook.es>
2022-02-15 09:25:03 +0100

    Changing from a set of objects to a map

--- main.tf
+++ main.tf
@@ -85,7 +85,8 @@ locals {
 # I am using here Terraform syntax with for_each and each.value to loop over the parameters of each json file parsed from locals.
 
 resource "dns_a_record_set" "www" {
-  for_each = toset(local.inputs)
+#  for_each = toset(local.inputs)
+  for_each = {for record in local.inputs: record.addresses => record}
   zone = var.zone
   name = "www"
   addresses = each.value.addresses
